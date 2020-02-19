727d270272e2235775760541c2ccdca1ed765468
Kumar Shanu <kumarshanu1009@gmail.com>
2020-02-19 15:23:39 +0530

    Adding single prediction with CNN

--- 2_Convolution_Neural_Networks/CNN.py
+++ 2_Convolution_Neural_Networks/CNN.py
@@ -57,3 +57,17 @@ classifier.fit_generator(training_set,
                          epochs=25,
                          validation_data=test_set,
                          nb_val_samples=2000)
+
+# Part 3 - Making new Prediction
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg',
+                            target_size=(64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis=0)
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = "Dog"
+else:
+    prediction = "Cat"
